034de90aa486dca6b26e0edb1a34e77abe28f4e5
Lucas Nestler <lucasnestler@web.de>
2020-02-21 22:23:31 +0100

    Expose BlockBlock

--- libs/__init__.py
+++ libs/__init__.py
@@ -7,3 +7,4 @@ from .models import Discriminator, Generator
 from .spectral_norm import SpectralNorm
 from .utils import (flatten, get_dataloader, get_dataset, get_model, hinge,
                     parameter_count, plot_hist, plot_images, prod)
+from .block import BlockBlock
